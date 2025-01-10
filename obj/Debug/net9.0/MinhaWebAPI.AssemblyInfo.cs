f97f86f9c78cd765dfeb9e3868e515b93ae73b58
Moises Nicolau <moises20010@gmail.com>
2025-01-10 09:19:14 -0300

    Atualiza a versão informacional da assembly para refletir as últimas alterações

--- obj/Debug/net9.0/MinhaWebAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/MinhaWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc09b20037f9339cff3b9122fd2c3e04b59a67e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a46ad70ad59d8ce98e8ebafe765638657905da")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
